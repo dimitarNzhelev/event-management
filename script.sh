de7b01de77a89fef264020ae3b5082f992132e93
Dimitar Zhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2024-10-31 14:35:12 +0200

    working ingress nginx controller

--- script.sh
+++ script.sh
@@ -1,11 +1,16 @@
-
 #!/bin/bash
 
 helm install url-shortener ./url-shortener-chart
 
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
+
+helm install nginx-ingress ingress-nginx/ingress-nginx --set controller.watchNamespace=""
 
 helm upgrade --install -f monitoring/values.yaml kube-prometheus-stack prometheus-community/kube-prometheus-stack -n monitoring
 
 kubectl apply -f monitoring/service-monitors/
+kubectl apply -f ingress/ingress.yaml
+
+minikube addons enable ingress
