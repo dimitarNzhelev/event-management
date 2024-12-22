ecad39b0f50de3b5ece3e3c897d1d336b359ab07
Dimitar Zhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2024-12-22 15:41:39 +0200

    deleted unused lines of code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,3 @@
-# Filter out local zones, which are not currently supported 
-# with managed node groups
-data "aws_availability_zones" "available" {
-  filter {
-    name   = "opt-in-status"
-    values = ["opt-in-not-required"]
-  }
-}
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.8.1"
